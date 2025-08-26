577e9b4aeb9dca46be1a728a877a38b1edeab3b3
Krzysztof Kuczewski <k.kuczewski10@gmail.com>
2025-08-26 20:10:18 +0200

    Added a full scan script

---
+++ .scripts/full-scan.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ip_cidr=$(ip a show "${INTERFACE}" | grep -Eo 'inet ([0-9]*\.){3}[0-9]*/[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*/[0-9]*' | head -n 1)
+
+nmap -p- -T4 "${ip_cidr}" -v
+
+curl --interface ${INTERFACE} ipinfo.io
+echo -e "\n"
+curl --interface ${INTERFACE} ifconfig.xyz
---
+++ full-scan.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+.scripts/full-scan.sh 2>&1 | tee logs/full_scan.log
